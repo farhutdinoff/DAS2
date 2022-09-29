4d51590e45b1bcddd1a641b7a8f9d32a4eebc62a
farhutdinoff <33476367+farhutdinoff@users.noreply.github.com>
2022-09-29 14:31:14 +0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -11,6 +11,34 @@ $('.owl-carousel-2').owlCarousel({
         }
     }
 })
+$('.owl-carousel-3').owlCarousel({
+    loop:true,
+    margin:30,
+    dots:false,
+    nav: true,
+    responsive:{
+        0:{
+            items:1
+        },
+        800:{
+            items:3
+        }
+    }
+})
+$('.owl-carousel-4').owlCarousel({
+    loop:true,
+    margin:30,
+    nav:true,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        800:{
+            items:4
+        }
+    }
+})
 
 $('.owl-carousel-6').owlCarousel({
     loop:true,
